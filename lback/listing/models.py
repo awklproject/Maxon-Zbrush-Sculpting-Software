6d5714212134a5c80a57601abffa7de65583a9da
awk <abkhlaf1@gmail.com>
2024-02-21 14:32:05 +0200

    adding new items to the model

--- lback/listing/models.py
+++ lback/listing/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+
 class OfferInfo(models.Model):
+    # supplier info
     supplier_name = models.CharField(max_length=100)
+    supplier_location = models.CharField(max_length=100)
+    supplier_address = models.CharField(max_length=100)
+    supplier_phone = models.CharField(max_length=100)
+
     description = models.TextField()
+    additional_info = models.TextField()
+    including = models.TextField()
+    excluding = models.TextField()
+
+    faq = models.TextField()
     unit = models.CharField(max_length=120)
     stock = models.IntegerField()
-    price_per_unit =  models.IntegerField()
+    price_per_unit = models.IntegerField()
 
     # google based informations
     # TODO : there must be some checks to make sure the links are correct
@@ -14,5 +25,5 @@ class OfferInfo(models.Model):
     drop_off_point = models.URLField()
     review = models.URLField()
 
-    # descriptive image
-    pic = models.ImageField(upload_to = 'uploads/')
+    pic = models.ImageField(upload_to='uploads/')
+
